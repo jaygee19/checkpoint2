0da6073e12a2c31f7b6f5e0d9e04fabe259fe709
jaygee19 <jan.gajdosik19@gmail.com>
2020-11-22 18:33:15 +0100

    22.11.

--- App/Views/Vote/edit.view.php
+++ App/Views/Vote/edit.view.php
@@ -3,9 +3,10 @@
     <h4> Edit song: (<?= $data->getArtist() ?> - <?= $data->getNameTrack() ?>) </h4>
     <p></p>
     <form action="?c=Vote&a=postEdit" method="post">
-        <input type="text" name="artist" value="<?= $data->getArtist() ?>">
-        <input type="text" name="nameTrack" value="<?= $data->getNameTrack() ?>">
+        <input type="text" name="artist" value="<?= $data->getArtist() ?>" required>
+        <input type="text" name="nameTrack" value="<?= $data->getNameTrack() ?>" required>
         <input type="hidden" name="id" value="<?= $data->getId() ?>">
-        <input type="submit" value="Send" >
+        <input type="submit" value="Send">
     </form>
 </div>
+
